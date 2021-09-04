a40f40ed76b118f3b830841dcf3fe6fb41f026ed
Imtiaz Epu <imtiazepu@programmer.net>
2021-09-04 17:16:08 +0600

    asso array intro

---
+++ php05.php
@@ -0,0 +1,27 @@
+<?php
+
+$foods = [
+  'vagitablea' =>'brrnjal, brocoli, carrot, capsicam',
+  'fruite' =>'orange, banana, apple',
+  'drink'=> 'water,milk',
+];
+
+$foods['drink'] .=", oreng juice";
+
+//echo $foods['vagitablea'];
+
+// foreach($foods as $key =>$value){
+//     echo $key."=". $value;
+// }
+
+// $keys = array_keys($foods);
+// for($i = 0; $i<count($keys); $i++){
+//     echo $foods[$keys[$i]];
+//     echo PHP_EOL;
+// }
+
+$value = array_values($foods);
+for($i = 0; $i<count($value); $i++){
+    echo $value[$i];
+    echo PHP_EOL;
+}
