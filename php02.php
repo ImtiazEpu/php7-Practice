b16e6f9739cfa528eac64584b4b19fde3fec6a4b
Imtiaz Epu <imtiazepu@programmer.net>
2021-09-04 13:40:41 +0600

    variable scope

--- php02.php
+++ php02.php
@@ -1,10 +1,42 @@
 <?php
 
-$name = "Earth";
+// $name = "Earth"; //global scope
 
-function doSomething(){
-    global $name;
-    echo $name;
+// function doSomething(){
+//     global $name;
+//     echo $name;
+// }
+
+// doSomething();
+
+// function doSomething() {
+//     //global $name;
+//     $name = "Earth"; //Local Scope
+//     //echo $name;
+// }
+
+//doSomething();
+//echo $name;
+
+function doSomething() {
+    static $i; //static scope
+    $i = $i ?? 0;
+    $i++;
+    echo $i;
+    echo PHP_EOL;
+}
+function doSomethingExtra() {
+    static $i; //static scope
+    $i = $i ?? 0;
+    $i++;
+    echo $i;
+    echo PHP_EOL;
 }
 
+doSomething();
+doSomething();
+doSomething();
+doSomething();
+doSomethingExtra();
+doSomethingExtra();
 doSomething();
