5859785f7d08fe397a35aa95c02b314b90c663b6
Imtiaz Epu <imtiazepu@programmer.net>
2021-09-08 00:31:40 +0600

    code refactoring

--- php05.php
+++ php05.php
@@ -1,12 +1,12 @@
 <?php
 
 $foods = [
-  'vagitablea' =>'brrnjal, brocoli, carrot, capsicam',
-  'fruite' =>'orange, banana, apple',
-  'drink'=> 'water,milk',
+    'vagitablea' => 'brrnjal, brocoli, carrot, capsicam',
+    'fruite'     => 'orange, banana, apple',
+    'drink'      => 'water,milk',
 ];
 
-$foods['drink'] .=", oreng juice";
+$foods['drink'] .= ", oreng juice";
 
 //echo $foods['vagitablea'];
 
@@ -20,8 +20,8 @@ $foods['drink'] .=", oreng juice";
 //     echo PHP_EOL;
 // }
 
-$value = array_values($foods);
-for($i = 0; $i<count($value); $i++){
+$value = array_values( $foods );
+for ( $i = 0; $i < count( $value ); $i++ ) {
     echo $value[$i];
     echo PHP_EOL;
 }
--- php11.php
+++ php11.php
@@ -1,6 +1,7 @@
 <?php
 $fruites = ['apple', 'banana', 'orange', 'plum', 'dates', 'mango'];
-$newFruits =['jackfruit','tanarid','pinapple'];
-$someFruites = array_splice($fruites,-5,2,$newFruits);
-print_r($someFruites);
-print_r($fruites);
+$newFruits = ['jackfruit', 'tanarid', 'pinapple'];
+
+$someFruites = array_splice( $fruites, -5, 2, $newFruits );
+print_r( $someFruites );
+print_r( $fruites );
