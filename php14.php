28bee8bb08bb602fe5c99eada91b70a364c51791
Imtiaz Epu <imtiazepu@programmer.net>
2021-09-08 16:48:17 +0600

    php search functions

---
+++ php14.php
@@ -0,0 +1,21 @@
+<?php
+
+$fruitsNew = ['a' => 'apple', 'b' => 'banana', 'c' => 'orange', 'd' => 'plum', 'e' => 'dates', 'g' => 'mango', 'f' => 'dates'];
+$numbers = [11, 23, 33, 4, 5, 7, 2, 16, 77, 22, 3, 50];
+
+if ( in_array( 23, $numbers, true ) ) {
+    echo "Data is found";
+} else {
+    echo "Data Not Found";
+}
+
+echo PHP_EOL;
+
+$offset = array_search( 77, $numbers );
+print_r( $offset );
+
+echo PHP_EOL;
+
+if ( key_exists( 'c', $fruitsNew ) ) {
+    echo "key b Exits";
+}
